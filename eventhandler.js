ac10f2e0f753bdb9482e1f7ff539a01b2a027ed9
Hoegsgaard <mathiashoegsgaard@gmail.com>
2018-06-05 16:46:10 +0200

    Min eOplysninger

--- eventhandler.js
+++ eventhandler.js
@@ -20,6 +20,7 @@ $(function() {
         yes = JSON.parse(this.responseText);
           if(document.getElementById("bruger").value ==yes[0].id){
             if(document.getElementById("pass").value== yes[0].id){
+                //alert(this.responseText);
                 $("#maincontainer").load("mainPage.html")
             }else{
               alert("invalid info")
