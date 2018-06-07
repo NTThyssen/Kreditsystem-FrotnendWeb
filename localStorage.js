2ac84adcae11a47b0fd8c2daa53cc8d4c06edbfd
Hoegsgaard <mathiashoegsgaard@gmail.com>
2018-06-07 14:17:42 +0200

    localStorage

--- localStorage.js
+++ localStorage.js
@@ -2,3 +2,4 @@ localStorage.setItem("id", "1");
 localStorage.setItem("firstname", "Hans");
 localStorage.setItem("lastname", "Gretesen");
 localStorage.setItem("balance",500);
+localStorage.setItem("adrasse","DTU");
